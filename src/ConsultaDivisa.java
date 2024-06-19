6f8ab853681622d2299374d24bc84c0673be2da1
victorcruvz <victorcruvz@gmail.com>
2024-06-18 18:50:25 -0600

    finalizado

---
+++ src/ConsultaDivisa.java
@@ -0,0 +1,25 @@
+import com.google.gson.Gson;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsultaDivisa {
+    public Divisa buscaDivisa(String tipoDivisa){
+        URI direccion = URI.create("https://v6.exchangerate-api.com/v6/0fe036925fb711ca4a9de4f3/latest/"+tipoDivisa);
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(direccion)
+                .build();
+
+        try {
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            return new Gson().fromJson(response.body(), Divisa.class);
+        } catch (Exception e) {
+            throw new RuntimeException("No se encontro la divisa");
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,80 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner lectura = new Scanner(System.in);
+        Calculo calculo = new Calculo();
+        int seleccion;
+        double cantidadConvertir;
 
+        //Presentacion
+        System.out.println("Bienvenido a este conversor de Divisas\n\n");
+
+        do {
+            //Menu
+            System.out.println("******************************************************");
+            System.out.println("Seleccione el tipo de conversion que desea realizar\n");
+            System.out.println("1) Dolar    ->>    P. mxn");//MXN
+            System.out.println("2) P. mxn   ->>    Dolar");
+            System.out.println("3) Dolar    ->>    R. brl");//BRL
+            System.out.println("4) R. brl   ->>    Dolar");
+            System.out.println("5) Dolar    ->>    P. cop");//COP
+            System.out.println("6) P. cop   ->>    Dolar");
+            System.out.println("7) SALIR");
+            System.out.println("******************************************************\n");
+            seleccion = Integer.valueOf(lectura.nextLine());
+
+            if (0<seleccion && seleccion<7){
+                //Monto a convertir
+                System.out.print("Ingresa la cantidad que quieres convertir: $");
+                cantidadConvertir = Double.valueOf(lectura.nextLine());
+                calculo.setCantidadConvertir(cantidadConvertir);
+            }
+
+            switch (seleccion){
+                case 1:
+                    calculo.setTipoDivisa("USD");
+                    calculo.setDivisaAConvertir("MXN");
+                    calculo.CalculoFinal();
+                    break;
+                    case 2://MXN
+                        calculo.setTipoDivisa("MXN");
+                        calculo.setDivisaAConvertir("USD");
+                        calculo.CalculoFinal();
+                        break;
+                    case 3:
+                        calculo.setTipoDivisa("USD");
+                        calculo.setDivisaAConvertir("BRL");
+                        calculo.CalculoFinal();
+                        break;
+                    case 4:
+                        calculo.setTipoDivisa("BRL");
+                        calculo.setDivisaAConvertir("USD");
+                        calculo.CalculoFinal();
+                        break;
+                    case 5:
+                        calculo.setTipoDivisa("USD");
+                        calculo.setDivisaAConvertir("COP");
+                        calculo.CalculoFinal();
+                        break;
+                    case 6:
+                        calculo.setTipoDivisa("COP");
+                        calculo.setDivisaAConvertir("USD");
+                        calculo.CalculoFinal();
+                        break;
+                    case 7:
+                        System.out.println("Hasta luego, vuelva pronto");
+                        break;
+                    default:
+                        System.out.println("Ingrese una opcion valida\n");
+                        break;
+                }
+
+
+        }while (seleccion!=7);
     }
+
 }
